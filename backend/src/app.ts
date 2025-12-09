b5843294a891ee73e5d5ee32424605f6b019a01b
kpogodzinski <kacperpogodzinski@spoko.pl>
2025-12-09 11:24:54 +0100

    Added /schedule route with doctorId parameter that returns doctor's schedule

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
+import scheduleRoutes from "./routes/schedule.routes";
 // import doctorRoutes from "./routes/doctor.routes"
 // import patientRoutes from "./routes/patient.routes"
 import dotenv from 'dotenv';
@@ -14,7 +15,8 @@ app.use(express.json());
 
 // Routes
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
+app.use("/schedule", scheduleRoutes);
 // app.use("/api/doctors", doctorRoutes)
 // app.use("/api/patients", patientRoutes)
 
---
+++ backend/src/services/schedule.service.ts
@@ -0,0 +1,16 @@
+import { prisma } from "../db/prisma";
+
+export class ScheduleService {
+    static async getSchedule(doctorId: number) {
+        return await prisma.schedule.findMany({
+            where: {
+                doctor_id: doctorId
+            },
+            select: {
+                day_of_the_week: true,
+                hour_from: true,
+                hour_to: true
+            }
+        });
+    }
+}
