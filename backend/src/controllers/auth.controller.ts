464c42ac0a3f4e107aa5039b2d597781984aea68
Kacper Szamborowski <kacperszamborowski@users.noreply.github.com>
2025-12-17 15:22:48 +0100

    Resolved conflicts (hopefully)

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -15,20 +15,11 @@ class AuthController {
         }
     }
 
-  async register(req: Request, res: Response) {
-    try {
-      const { firstname, lastname, birth_date, email, password } = req.body.payload;
-
-      const user = await authService.register(firstname, lastname, birth_date, email, password);
-      res.status(201).json(user);
-    } 
-    catch (e: any) {
-      res.status(400).json({ error: e.message });
     async register(req: Request, res: Response) {
         try {
-            const { firstname, lastname, birth_date, email, password, role } = req.body;
+            const { firstname, lastname, birth_date, email, password } = req.body.payload;
 
-            const user = await authService.register(firstname, lastname, birth_date, email, password, role);
+            const user = await authService.register(firstname, lastname, birth_date, email, password);
             res.status(201).json(user);
         }
         catch (e: any) {
--- backend/src/routes/__tests__/auth.routes.test.ts
+++ backend/src/routes/__tests__/auth.routes.test.ts
@@ -12,45 +12,16 @@ describe('Auth Endpoints', () => {
         });
     });
 
-  it('should register a new user', async () => {
-    const res = await request(app)
-      .post('/auth/register')
-      .send({
-        firstname: 'test',
-        lastname: 'example',
-        birth_date: (new Date('1990-07-16')).toISOString(),
-        email: 'test1@example.com',
-        password: '!password123',
-      });
-
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toHaveProperty('id');
-  });
-
-  it('should login and return JWT token', async () => {
-    const res = await request(app)
-      .post('/auth/login')
-      .send({
-        email: 'jan.kowalski@example.com',
-        password: 'pass123'
-      });
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('token');
-    token = res.body.token;
-    console.log(token)
-  });
     it('should register a new user', async () => {
         const res = await request(app)
-        .post('/auth/register')
-        .send({
-            firstname: 'test',
-            lastname: 'example',
-            birth_date: (new Date('1990-07-16')).toISOString(),
-            email: 'test1@example.com',
-            password: '!password123',
-            role: 'patient'
-        });
+            .post('/auth/register')
+            .send({
+                firstname: 'test',
+                lastname: 'example',
+                birth_date: (new Date('1990-07-16')).toISOString(),
+                email: 'test1@example.com',
+                password: '!password123',
+            });
 
         expect(res.statusCode).toEqual(201);
         expect(res.body).toHaveProperty('id');
@@ -58,31 +29,33 @@ describe('Auth Endpoints', () => {
 
     it('should login and return JWT token', async () => {
         const res = await request(app)
-        .post('/auth/login')
-        .send({
-            email: 'jan.kowalski@example.com',
-            password: 'pass123'
-        });
+            .post('/auth/login')
+            .send({
+                email: 'jan.kowalski@example.com',
+                password: 'pass123'
+            });
 
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('token');
+        token = res.body.token;
+        console.log(token)
     });
 
     it('should change password for user with id=1', async () => {
         const auth = await request(app)
-        .post('/auth/login')
-        .send({
-            email: 'jan.kowalski@example.com',
-            password: 'pass123'
-        });
-        
+            .post('/auth/login')
+            .send({
+                email: 'jan.kowalski@example.com',
+                password: 'pass123'
+            });
+
         const res = await request(app)
-        .put("/auth/password")
-        .send({
-            oldPassword: "pass123",
-            newPassword: "test789"
-        })
-        .set("Authorization", "Bearer " + auth.body.token);
+            .put("/auth/password")
+            .send({
+                oldPassword: "pass123",
+                newPassword: "test789"
+            })
+            .set("Authorization", "Bearer " + auth.body.token);
 
         const user = await prisma.user.findUnique({
             where: { id: 1 },
@@ -105,28 +78,28 @@ describe('Auth Endpoints', () => {
 
     it('should fail to change password', async () => {
         const res1 = await request(app)
-        .put("/auth/password")
-        .send({
-            oldPassword: "pass123",
-            newPassword: "test789"
-        });
+            .put("/auth/password")
+            .send({
+                oldPassword: "pass123",
+                newPassword: "test789"
+            });
 
         expect(res1.status).toBe(401);
-        
+
         const auth = await request(app)
-        .post('/auth/login')
-        .send({
-            email: 'jan.kowalski@example.com',
-            password: 'pass123'
-        });
-        
+            .post('/auth/login')
+            .send({
+                email: 'jan.kowalski@example.com',
+                password: 'pass123'
+            });
+
         const res2 = await request(app)
-        .put("/auth/password")
-        .send({
-            oldPassword: "wrongPass",
-            newPassword: "test789"
-        })
-        .set("Authorization", "Bearer " + auth.body.token);
+            .put("/auth/password")
+            .send({
+                oldPassword: "wrongPass",
+                newPassword: "test789"
+            })
+            .set("Authorization", "Bearer " + auth.body.token);
 
         expect(res2.status).toBe(400);
     });
